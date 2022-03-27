b465a57243b17c5a1e64a84d3f2fffa501d0c455
dhakad0502 <100011014+dhakad0502@users.noreply.github.com>
2022-03-27 13:59:22 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,12 @@
+def buildApp(){
+    echo 'Building the appliccation'
+}
+def testApp(){
+    echo 'Building the appliccation'
+}
+def deployApp(){
+    echo 'Building the appliccation'
+	echo "Building the appliccation ${params.VERSION}"
+}
+
+return this
